16d2be3a3c7b097d9fca3d0291a0df0b9683efd4
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-26 14:22:38 +0200

    Fix the key bug

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import Todo from './Todo';
+import nextId from 'react-id-generator';
 
 const listStyle = css`
   display: flex;
@@ -16,27 +17,30 @@ const listStyle = css`
 //for the final else part the filtering returns all of the items, which are then mapped over to return all of the items returned by the filter. WOWWWWWW
 
 export default function TodoList(props) {
-  return props.todos
-    .filter((item) => {
-      if (props.filter === 'active') {
-        return item.complete !== true;
-      } else if (props.filter === 'completed') {
-        return item.complete === true;
-      } else {
-        return true;
-      }
-    })
+  return (
+    <div>
+      {props.todos
+        .filter((item) => {
+          if (props.filter === 'active') {
+            return item.complete !== true;
+          } else if (props.filter === 'completed') {
+            return item.complete === true;
+          } else {
+            return true;
+          }
+        })
 
-    .map((item) => {
-      return (
-        <div css={listStyle}>
-          <Todo
-            todo={item}
-            key={item.id}
-            crossTodo={props.crossTodo}
-            removeTodo={props.removeTodo}
-          />
-        </div>
-      );
-    });
+        .map((item) => {
+          return (
+            <div css={listStyle} key={item.id}>
+              <Todo
+                todo={item}
+                crossTodo={props.crossTodo}
+                removeTodo={props.removeTodo}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
 }
